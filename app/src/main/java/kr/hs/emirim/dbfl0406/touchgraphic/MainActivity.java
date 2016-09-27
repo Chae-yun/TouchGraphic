5e1a1eb07e854902e69c82d37e58cf7ddb4001bb
Chaeyun Yang <ycy6685@naver.com>
2016-09-27 20:57:23 +0900

    #1 Make Project

---
+++ app/src/main/java/kr/hs/emirim/dbfl0406/touchgraphic/MainActivity.java
@@ -0,0 +1,32 @@
+package kr.hs.emirim.dbfl0406.touchgraphic;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new DrawShape(getApplicationContext()));
+    }
+
+    class DrawShape extends View {
+        DrawShape(Context context){
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            float cx=getWidth()/2.0f;
+            float cy=getHeight()/2.0f;
+            Paint paint=new Paint();
+            canvas.drawCircle(cx,cy,50,paint);
+        }
+    }
+}
