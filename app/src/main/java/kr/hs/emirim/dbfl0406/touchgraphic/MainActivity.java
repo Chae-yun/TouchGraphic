cfd98621a0f1e5196d4574ea802ebf0d39fe35d6
Chaeyun Yang <ycy6685@naver.com>
2016-09-29 19:10:40 +0900

    #2 Change Layout

--- app/src/main/java/kr/hs/emirim/dbfl0406/touchgraphic/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbfl0406/touchgraphic/MainActivity.java
@@ -2,6 +2,7 @@ package kr.hs.emirim.dbfl0406.touchgraphic;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -26,7 +27,13 @@ public class MainActivity extends AppCompatActivity {
             float cx=getWidth()/2.0f;
             float cy=getHeight()/2.0f;
             Paint paint=new Paint();
-            canvas.drawCircle(cx,cy,50,paint);
+            paint.setStrokeWidth(7);
+            paint.setColor(Color.MAGENTA);
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawCircle(cx,cy,100,paint);
+            paint.setColor(Color.BLUE);
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawRect(200,200,500,450,paint); //300*250
         }
     }
 }
